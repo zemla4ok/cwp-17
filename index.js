781e8ce85ef3d918a0f6a9e17074b2fce52a4ffe
zemla4ok <dima-vital@live.com>
2017-12-19 23:01:35 +0300

    installed sharp

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const multer  = require('multer');
+const sharp = require('sharp');
+
+const storage = multer.diskStorage({
+  destination: './uploads/',
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+
+const upload = multer({ storage: storage });
+
+const app = express();
+
+app.post('/upload', upload.single('file'), (req, res, next) => {
+  res.json({ succeed: true });
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
