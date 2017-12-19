2e35b95528493e1caee9a8c1a2670e7bf01e8e54
zemla4ok <dima-vital@live.com>
2017-12-19 23:59:36 +0300

    task 3

--- index.js
+++ index.js
@@ -46,7 +46,36 @@ app.post('/pdf', pdfUpload.array('files', 3), (req, res, next) => {
     res.json({files: files.map((file) => file = file.filename)});
 });
 
+//images
+const imageStorage = multer.diskStorage({
+    destination: './uploads/images/',
+    filename: function (req, file, cb) {
+        cb(null, uuid() + "-master" + file.originalname.slice(-4));
+    }
+});
 
+const imageUpload = multer({
+    storage: imageStorage,
+    fileFilter: function fileFilter(req, file, cb) {
+        file.originalname.toUpperCase().endsWith(".JPG") || file.originalname.toUpperCase().endsWith(".PNG")
+            ? cb(null, true) : cb(null, false);
+    }
+});
 
+app.get('/images', function (req, res) {
+    res.sendFile('image.html', {root: './public/'});
+});
+
+app.post('/images', imageUpload.single('image'), async (req, res, next) => {
+    let regexp = /-master\.[^\.]+$/;
+    let filenames = [
+        req.file.filename.replace(regexp, "-preview" + req.file.filename.slice(-4)),
+        req.file.filename.replace(regexp, "-thumbnail" + req.file.filename.slice(-4)),
+        req.file.filename
+    ];
+    await sharp('./uploads/images/' + req.file.filename).resize(800, 600).toFile('./uploads/images/' + filenames[0]);
+    await sharp('./uploads/images/' + req.file.filename).resize(300, 180).toFile('./uploads/images/' + filenames[1]);
+    res.json(filenames);
+});
 
 app.listen(3000, '127.0.0.1', () => console.log('Example app listening on port 3000!'));
