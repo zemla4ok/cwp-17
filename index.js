b58f89e55a2b6d8d3049cb43cdb66f741282d9f6
zemla4ok <dima-vital@live.com>
2017-12-20 15:02:32 +0300

    tests

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const pdfStorage = multer.diskStorage({
 const pdfUpload = multer({
     storage: pdfStorage,
     fileFilter: function fileFilter(req, file, cb) {
-        file.originalname.toUpperCase().endsWith(".PDF") ? cb(null, true) : cb(null, false);
+        file.mimetype.split('/')[1] === 'pdf' ? cb(null, true) : cb(null, false);
     }
 });
 
@@ -57,8 +57,7 @@ const imageStorage = multer.diskStorage({
 const imageUpload = multer({
     storage: imageStorage,
     fileFilter: function fileFilter(req, file, cb) {
-        file.originalname.toUpperCase().endsWith(".JPG") || file.originalname.toUpperCase().endsWith(".PNG")
-            ? cb(null, true) : cb(null, false);
+        file.mimetype.split('/')[0] === 'image' ? cb(null, true) : cb(null, false);
     }
 });
 
@@ -67,11 +66,11 @@ app.get('/images', function (req, res) {
 });
 
 app.post('/images', imageUpload.single('image'), async (req, res, next) => {
-    let regexp = /-master\.[^\.]+$/;
+    let currFile = req.file.filename.slice(0, 37) ;
     let filenames = [
-        req.file.filename.replace(regexp, "-preview" + req.file.filename.slice(-4)),
-        req.file.filename.replace(regexp, "-thumbnail" + req.file.filename.slice(-4)),
-        req.file.filename
+        currFile,
+        currFile + 'preview',
+        currFile + 'thumbnail'
     ];
     await sharp('./uploads/images/' + req.file.filename).resize(800, 600).toFile('./uploads/images/' + filenames[0]);
     await sharp('./uploads/images/' + req.file.filename).resize(300, 180).toFile('./uploads/images/' + filenames[1]);
